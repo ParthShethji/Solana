f0547d6e9c6b63489aa99c6f724ab63beb0d6513
Parth Shethji <parthshethji2693@gmail.com>
2025-10-15 22:03:37 +0530

    Account upgraded to use PDAs

--- anchor-counter/programs/anchor-counter/src/lib.rs
+++ anchor-counter/programs/anchor-counter/src/lib.rs
@@ -9,6 +9,7 @@ pub mod anchor_counter {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
         counter.count = 0;
+        counter.bump = ctx.bumps.counter;
         msg!("Counter Account Created");
         msg!("Current Count: {}", counter.count);
         Ok(())
@@ -21,27 +22,40 @@ pub mod anchor_counter {
         Ok(())
     }
 }
-
+//  Define the accounts involved in the instructions
+//  These are the format the payload is expected to be in
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = DISCRIMINATOR + Counter::INIT_SPACE)]
+    // creating account counter if it doesn't exist
+    #[account(init,
+        payer = user,
+        seeds =[b"counter"],
+        bump,
+        space = DISCRIMINATOR + Counter::INIT_SPACE)]
     pub counter: Account<'info, Counter>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
-
+//  Define the accounts involved in the instructions
+//  These are the format the payload is expected to be in
 #[derive(Accounts)]
 pub struct Update<'info> {
-    #[account(mut)]
+    #[account(
+        mut,
+        seeds = [b"counter"],
+        bump = counter.bump,
+    )]
     pub counter: Account<'info, Counter>,
     pub user: Signer<'info>,
 }
 
+// Define the Counter account structure
 #[account]
 #[derive(InitSpace)]
 pub struct Counter {
     pub count: u64,
+    pub bump: u8,
 }
 
 const DISCRIMINATOR: usize = 8;
--- anchor-counter/tests/anchor-counter.ts
+++ anchor-counter/tests/anchor-counter.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorCounter } from "../target/types/anchor_counter";
 import { expect } from "chai";
+import { PublicKey } from "@solana/web3.js";
 
 describe("anchor-counter", () => {
   // Configure the client to use the local cluster.
@@ -9,22 +10,24 @@ describe("anchor-counter", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.anchorCounter as Program<AnchorCounter>;
-  const counter = anchor.web3.Keypair.generate();
+  // const counter = anchor.web3.Keypair.generate();
+  const[counterpda] = PublicKey.findProgramAddressSync([Buffer.from("counter")], program.programId);
   it("Is initialized!", async () => {
     const tx = await program.methods
     .initialize()
-    .accounts({counter: counter.publicKey})
-    .signers([counter])
     .rpc();
     console.log("Your transaction signature for initialisation is ", tx);
+    const account_data = await program.account.counter.fetch(counterpda);
+    console.log("The account data is ", account_data.count);
   });
 
   it("Incremented the count", async () => {
     const tx = await program.methods
     .update()
-    .accounts({counter: counter.publicKey})
     .rpc();
     console.log("Your transaction signature", tx);
+    const account_data = await program.account.counter.fetch(counterpda);
+    console.log("The account data is ", account_data.count);
   });
 });
 
